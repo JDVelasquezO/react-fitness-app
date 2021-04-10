e6e2dafccd3e029e921ab2646b3d1288380ed33a
Daniel Velasquez <jdvela158@gmail.com>
2021-04-10 10:32:57 -0600

    adding imgs and css

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import exerciseImg from '../images/exercise.png';
+import './styles/Card.css';
 
 class CardComponent extends React.Component {
     render() {
-        return <h1>Card</h1>;
+        return (
+            <div className='card mx-auto Fitness-Card'>
+                <div className='card-body'>
+                    <div className='row center'>
+                        <div className='col-6'>
+                            <img className='float-right' src={exerciseImg} />
+                        </div>
+                        <div className='col-6 Fitness-Card-Info'>
+                            <h1>Guías Tecnicas</h1>
+                            <p>Parrafo de prueba que dice cualquier cosa</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
-const user = {
-  first: 'JD',
-  last: 'Velasquez',
-  avatar: 'https://image.freepik.com/vector-gratis/perfil-avatar-hombre-icono-redondo_24640-14044.jpg'
-};
-
-function getName (user) {
-  return `${user.first} ${user.last}`;
-}
-
-function getGreeting(user) {
-  if (user) {
-    return <h1>Hola a {getName(user)}</h1>;
-  }
-  return <h1>Hola amigo</h1>;
-}
-
-const element = (
-  <div>
-    {getGreeting(user)}
-    <img src={user.avatar} width='200px' />
-  </div>
-);
+import CardComponent from './components/CardComponent';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const container = document.getElementById('root');
-ReactDom.render(element, container);
+ReactDom.render(<CardComponent />, container);
